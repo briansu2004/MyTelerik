bb184a1ceb7f0361f6df95850eee38abd2f10ad6
Brian Su <briansu2004@hotmail.com>
2022-03-26 11:10:30 -0400

    Telerik AutoComplete Blazor

---
+++ TelerikUI4Blazor/Telerik-AutoComplete-Blazor/Telerik-AutoComplete-Blazor/Dtos/CategoryDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelerikAutoComplete_simple.Dto
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(150)]
+        public string PictureUrl { get; set; }
+
+        public List<ProductDto> Products { get; set; }
+
+    }
+}
